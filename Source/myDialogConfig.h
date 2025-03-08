9c21d7965f2496d192b71e53fb9a2434bd833d5d
shspage <shspage@gmail.com>
2025-03-08 18:47:29 +0900

    update imgui, myImGuiDialog_win, and build event

--- Source/myDialogConfig.h
+++ Source/myDialogConfig.h
@@ -6,7 +6,7 @@ static const float kMyDialogWidth = 320.0f;
 #ifdef __APPLE__
 static const float kMyDialogHeight = 120.0f;
 #else
-static const float kMyDialogHeight = 200.0f;
+static const float kMyDialogHeight = 160.0f;
 #endif
 
 static const char* kMyDialogTitle = "AnchorGrabber";
